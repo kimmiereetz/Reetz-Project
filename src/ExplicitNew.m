c0b5ff048b84ca13d7833721c8e269aa4fcf8614
kimmiereetz <kimmiereetz@gmail.com>
2017-05-05 20:18:43 -0500

    Revisions and final comments

--- src/ExplicitNew.m
+++ src/ExplicitNew.m
@@ -1,20 +1,33 @@
 clear all
 clc
+% Setting up grid number of nodes N determines the fine-ness of the mesh
+% the larger N the finer the mesh
 
 N=40;
-    
+% Based on the number of nodes the increments for dx dy and dt are
+% determined    
 dx=2*pi/(N-1);
-
+% Stability condition enforced
 dt=.9*dx^2/4;
+% Based on the increment size for the time and space dimensions and their
+% intervals the vectors for x, y, and time are created
 t=0:dt:30;
 y=0:dx:2*pi;
 x=y;
+% The constants determined from discritization are calculated befoe the
+% loop to optimize code
 
 k=dt/(dx^2);
 c=1-4*k;
+% For ease of operating loops the number of elements in each x y and time
+% vector are calculated
+nx=length(x);
+ts=length(t);
 
 ny=length(y);
-ts=length(t);
+% The Dirichlet boundary conditions were reduced and are evaluated to 
+% produce vectors.
+
 PHIab=cos(pi*y).*cosh(2*pi-y);
 PSIab=y.^2.*sin(y/4);
 % Creating the intial U grid
@@ -28,12 +41,14 @@ U=[PHIab;zeros(N-2,N);PSIab];
 % Allocating memory for variables
 Un=zeros(ny,ny);
 center=zeros(1,ts);
+% This mesh is required for plotting the surface solution 
 [X,Y]=meshgrid(x,y);
 
 
 %% 
-% caluclate variables
 
+% The loops for solving the problem are ready to run. Time is the outer
+% most loop folloeing by the y direction and x direction
 
     for h=1:ts;
         for j=1:ny;
@@ -46,11 +61,15 @@ center=zeros(1,ts);
                     Un(i,j)=k*U(i,j-1)+k*U(i-1,j)+c*U(i,j)+k*U(i+1,j)+k*U(i,j+1);
                 end
             end
+% The centrally located grid node is tracked for use of steady state
+% determination and diffusion confirmation
+           
             center(h)=Un(N/2,N/2);
         end
         U=[U(1,:); Un(2:end-1,:) ; U(end,:)];
     
-        
+% Throughout the time steps plots are generated to confirm diffusion
+       
          if mod(h,10)==0;
              figure (1)
              surf(X,Y,U)
@@ -61,7 +80,9 @@ center=zeros(1,ts);
 
 figure (2)
 plot(t,center)
-%gi=norm(U,2);
-
+%gi=norm(U,2);% The L2 Norm of the final U matrix is used for grid convergence. The L2
+% Norms at Nodes of N 2N 4N 16N are calculted until the solution truncates 
+% The minimum difference in L2 Norms is calculated to determine the sweet
+% spot for the numerical solution
 
 display('Done')
